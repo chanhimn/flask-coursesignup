6051996930a6848426a89a643e09a5da2facb0c0
chanhimn <chanhimn@gmail.com>
2020-03-12 10:22:33 +0800

    Update web_handler.py

--- web_handler.py
+++ web_handler.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
+app.debug = False
 
 @app.route('/')
 def index():
@@ -64,4 +65,4 @@ def get_queue_number(rid):
     return html
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)

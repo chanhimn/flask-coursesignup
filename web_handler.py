7e990c856c2e033f6e28aacc01cf50fc277f978a
chanhimn <chanhimn@gmail.com>
2020-04-08 17:33:48 +0800

    Update web_handler.py

--- web_handler.py
+++ web_handler.py
@@ -2,61 +2,61 @@ from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
-app.debug = False
+db_filename = "coursesignup.db"
 
 @app.route('/')
 def index():
-    db = sqlite3.connect('queuester.db')
-    results = db.execute("SELECT Queues.Id, Queues.QueueName, Queues.CurrentQueueNumber FROM Queues").fetchall()
+    db = sqlite3.connect(db_filename)
+    results = db.execute("SELECT Courses.Id, Courses.CourseName, Courses.CourseQuota FROM Courses").fetchall()
     records = []
     for result in results:
-        curnum = db.execute("SELECT COUNT (*) FROM QueueNumber WHERE Id = ?", (result[0],)).fetchone()[0]
+        num_signup = db.execute("SELECT COUNT (*) FROM SignUps WHERE Id = ?", (result[0],)).fetchone()[0]
         record = []
         record.extend(result)
-        record.append(curnum)
+        record.append(num_signup)
         records.append(record)
     db.close()
     return render_template('index.html', records = records)
 
-@app.route('/new_queue', methods = ['GET', 'POST'])
-def new_queue():
+@app.route('/new_course', methods = ['GET', 'POST'])
+def new_course():
     if request.method == 'POST':
-        db = sqlite3.connect('queuester.db')
-        db.execute("INSERT INTO Queues(QueueName, CurrentQueueNumber) VALUES (?, ?)", (request.form['queue_name'], request.form['current_queue_number']))
+        db = sqlite3.connect(db_filename)
+        db.execute("INSERT INTO Courses(CourseName, CourseQuota) VALUES (?, ?)", (request.form['course_name'], request.form['course_quota']))
         db.commit()
         db.close()
         return redirect(url_for('index'))
     else:
-        return render_template('new_queue.html')
+        return render_template('new_course.html')
 
-@app.route('/display_queue/<string:rid>', methods = ["GET", "POST"])
-def display_queue(rid):
-    db = sqlite3.connect('queuester.db')
-    queue_name = db.execute("SELECT Queues.QueueName FROM Queues WHERE Queues.Id = ?", (rid,)).fetchall()[0][0]       
-    records = db.execute("SELECT QueueNumber, Email FROM QueueNumber WHERE Id = ?", (rid,)).fetchall()
-    html = render_template('display_queue.html', queue_name=queue_name, records=records)
+@app.route('/display_participants/<string:rid>', methods = ["GET", "POST"])
+def display_participants(rid):
+    db = sqlite3.connect(db_filename)
+    course_name = db.execute("SELECT Courses.CourseName FROM Courses WHERE Courses.Id = ?", (rid,)).fetchall()[0][0]       
+    records = db.execute("SELECT ParticipantId, Email FROM SignUps WHERE Id = ?", (rid,)).fetchall()
+    html = render_template('display_participants.html', course_name=course_name, records=records)
     return html
 
 
-@app.route('/get_queue_number/<string:rid>', methods = ["GET", "POST"])
-def get_queue_number(rid):
+@app.route('/sign_up/<string:rid>', methods = ["GET", "POST"])
+def sign_up(rid):
     if request.method == 'GET':
-        db = sqlite3.connect('queuester.db')
-        records = db.execute("SELECT Id, QueueName, CurrentQueueNumber FROM Queues WHERE Id = ?", (rid,)).fetchall()
+        db = sqlite3.connect(db_filename)
+        records = db.execute("SELECT Id, CourseName, CourseQuota FROM Courses WHERE Id = ?", (rid,)).fetchall()
         print(records)
         if len(records) == 1:
-            html = render_template('get_queue_number.html', rid=rid, records=records)
+            html = render_template('sign_up.html', rid=rid, records=records)
         else:
-            html = "No such Queue Id."
+            html = "No such Course Id."
         db.close()
     else:
-        print(request.form['rid'], request.form['queue_name'], request.form['email'])
-        db = sqlite3.connect('queuester.db')
-        records = db.execute("SELECT Queues.CurrentQueueNumber FROM Queues WHERE Queues.Id = ?", (rid,)).fetchall()
-        curnum = db.execute("SELECT COUNT (*) FROM QueueNumber WHERE Id = ?", (rid,)).fetchone()[0]
+        print(request.form['rid'], request.form['course_name'], request.form['email'])
+        db = sqlite3.connect(db_filename)
+        records = db.execute("SELECT Courses.CourseQuota FROM Courses WHERE Courses.Id = ?", (rid,)).fetchall()
+        num_signups = db.execute("SELECT COUNT (*) FROM SignUps WHERE Id = ?", (rid,)).fetchone()[0]
         
-        if curnum < records[0][0]:
-            db.execute("INSERT INTO QueueNumber(Email, Id, QueueNumber) VALUES (?, ?, ?)", (request.form['email'], request.form['rid'], curnum+1))
+        if num_signups < records[0][0]:
+            db.execute("INSERT INTO SignUps(Email, Id, ParticipantId) VALUES (?, ?, ?)", (request.form['email'], request.form['rid'], num_signups+1))
             db.commit()
             html = redirect(url_for('index'))
         else:
